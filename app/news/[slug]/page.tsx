69cda3c74e4bb9dfe8557292407e320903ca726e
Fabian Hanso <fabian.hanso@accenture.com>
2025-06-17 00:11:36 +0200

    no unused code

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { fetchPostBySlug } from "@/lib/contentful";
 import { notFound } from "next/navigation";
