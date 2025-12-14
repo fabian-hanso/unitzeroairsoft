645627181a3475309d476dcbe0df34f299eee92f
Fabian Hanso <fabian.hanso@accenture.com>
2025-12-14 23:50:42 +0100

    comment

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,13 +1,13 @@
-import { getApps, initializeApp, cert } from "firebase-admin/app";
-import { getAuth } from "firebase-admin/auth";
-import admin from "firebase-admin";
+// import { getApps, initializeApp, cert } from "firebase-admin/app";
+// import { getAuth } from "firebase-admin/auth";
+// import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!);
+// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!);
 
-const app =
-  getApps().length === 0
-    ? initializeApp({ credential: cert(serviceAccount) })
-    : getApps()[0];
+// const app =
+//   getApps().length === 0
+//     ? initializeApp({ credential: cert(serviceAccount) })
+//     : getApps()[0];
 
-export const adminAuth = getAuth(app);
-export const firestore = admin.firestore();
+// export const adminAuth = getAuth(app);
+// export const firestore = admin.firestore();
