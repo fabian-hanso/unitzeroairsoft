8d442a360b8c1901f41c74a5c4562d477834d4ca
Fabian Hanso <fabian.hanso@accenture.com>
2025-12-14 23:53:12 +0100

    logic

--- app/(auth)/dashboard/(admin)/team-ansicht/page.tsx
+++ app/(auth)/dashboard/(admin)/team-ansicht/page.tsx
@@ -1,7 +1,6 @@
 import AdminTeamView from "@/components/AdminTeamView/AdminTeamView";
 import Breadcrumbs from "@/components/Breadcrumbs/Breadcrumbs";
 import React from "react";
-// Import admin SDK (adjust path as needed)
 
 export default async function page() {
   const breadcrumbs = [{ name: "Events", href: "/dashboard/events" }];
