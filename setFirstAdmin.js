16a3f0ed67ae562a1c9c1e7eb2a5241d2f1a734f
Fabian Hanso <fabian.hanso@accenture.com>
2025-12-14 23:50:03 +0100

    delete authentication

--- setFirstAdmin.js
+++ setFirstAdmin.js
@@ -1,24 +1,24 @@
-import admin from "firebase-admin";
-import { readFileSync } from "fs";
+// import admin from "firebase-admin";
+// import { readFileSync } from "fs";
 
-if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(
-    readFileSync("./serviceAccount.json", "utf-8")
-  );
+// if (!admin.apps.length) {
+//   const serviceAccount = JSON.parse(
+//     readFileSync("./serviceAccount.json", "utf-8")
+//   );
 
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//   });
+// }
 
-async function setFirstAdmin(email) {
-  try {
-    const user = await admin.auth().getUserByEmail(email);
-    await admin.auth().setCustomUserClaims(user.uid, { admin: true });
-    console.log(`${email} wurde erfolgreich zum Admin befördert.`);
-  } catch (err) {
-    console.error("Fehler:", err);
-  }
-}
+// async function setFirstAdmin(email) {
+//   try {
+//     const user = await admin.auth().getUserByEmail(email);
+//     await admin.auth().setCustomUserClaims(user.uid, { admin: true });
+//     console.log(`${email} wurde erfolgreich zum Admin befördert.`);
+//   } catch (err) {
+//     console.error("Fehler:", err);
+//   }
+// }
 
-setFirstAdmin("f.hanso@revice-media.com");
+// setFirstAdmin("f.hanso@revice-media.com");
